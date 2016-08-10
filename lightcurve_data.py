6e64f15639b9e2fd0fe45f4da85c33ada0c87855
Shanen Cross <shanencross@yahoo.com>
2016-08-09 18:07:46 -0700

    Basic features of classes are mostly working, including plotting measurement simulations for both baselines and lensing events

--- lightcurve_data.py
+++ lightcurve_data.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from astropy import units
 from simulating_mag_error import simulate_mag_error
 from lightcurve_simulation import get_magnified_mag
+from true_observation_time import get_true_observation_time
+from baseline_lightcurve_simulation import get_gaussian_mag_info
 
 IMPACT_MIN_DEFAULT = 0.1
 EINSTEIN_TIME_DEFAULT = 10 * units.d
@@ -15,7 +17,7 @@ TIME_MAX_DEFAULT = 15 * units.d
 DURATION_DEFAULT = 2 * TIME_MAX_DEFAULT
 PERIOD_DEFAULT = 17.7 * units.h
 NIGHT_DURATION_DEFAULT = 10 * units.h
-NIGHT_START_TIME_DEFAULT = 0 * units.h
+START_TIME_DEFAULT = 0 * units.h
 TIME_STEP_DEFAULT = 1*units.h
 TIME_UNIT_DEFAULT = units.d
 
@@ -25,32 +27,39 @@ BAND_DEFAULT = "V"
 MAG_DEFAULT = 25
 
 MAGNITUDE_BANDS = ["V", "B", "U", "I", "K", "u", "g", "r", "i", "z"]
+BAND_PLOT_COLOR_DICT = {"V": "y", "B": "b", "U": "m", "I": "k", "K": "r",
+                        "u": "b", "g": "g", "r": "r", "i": "m", "z": "k"}
 
 class Lightcurve_data():
     def __init__(self, star=None, mags=None, bands=None, impact_min=IMPACT_MIN_DEFAULT,
                 einstein_time=EINSTEIN_TIME_DEFAULT, time_max = TIME_MAX_DEFAULT,
                 duration=DURATION_DEFAULT, period=PERIOD_DEFAULT,
                 night_duration=NIGHT_DURATION_DEFAULT,
-                night_start_time=NIGHT_START_TIME_DEFAULT, time_step=TIME_STEP_DEFAULT,
+                start_time=START_TIME_DEFAULT, time_step=TIME_STEP_DEFAULT,
                 time_unit=TIME_UNIT_DEFAULT):
+        self.time_unit = time_unit
         self.star = star
         self.mags = self._init_mags(mags)
 
         self.mag_sigma_errors = self._init_sigma_errors() # (derived from mag)
 
         self.impact_min = impact_min # parameter for which smaller values mean larger magnification for lightcurve
-        self.time_max = time_max    # time when lightcurve peaks
-        self.einstein_time = einstein_time  # "width" of lightcurve
-        self.duration = duration    # length of time during which measurements are made
-        self.period = period    # time spent observing (i.e. nighttime) between each measurement
-        self.day_night_duration = DAY_NIGHT_DURATION    # length of day-night cycle; really should be a constant set to 24 hr
-        self.night_duration = night_duration    # length of time that night lasts; should not exceed day_night_duration
+        self.time_max = time_max.to(self.time_unit)    # time when lightcurve peaks
+        self.einstein_time = einstein_time.to(self.time_unit) # "width" of lightcurve
+        self.duration = duration.to(self.time_unit)    # length of time (total, including day and night) during which measurements are made
+        self.period = period.to(self.time_unit)    # time spent observing (i.e. nighttime) between each measurement
+        self.day_night_duration = DAY_NIGHT_DURATION.to(self.time_unit)    # length of day-night cycle; really should be a constant set to 24 hr
+        self.night_duration = night_duration.to(self.time_unit)    # length of time that night lasts; should not exceed day_night_duration
         self.day_duration = self.day_night_duration - self.night_duration   # calculated from day_night_duration - night_duration
+        self.start_time = start_time.to(self.time_unit) # Amount of observing time (i.e. night time -- NOT total time) before first measurement is taken;
+                                     # Values should be 0 or positive; negative values may not work as intended/expected;
+                                     # note that time 0 is always the beginning of a night
+        """
         self.night_start_time = night_start_time    # how long after time 0 night begins, ranging from
                                                     # -night_duration to +day_duration
                                                     # (both extremes are time 0 being the start of daylight)
-        self.time_step = time_step
-        self.time_unit = time_unit
+        """
+        self.time_step = time_step.to(self.time_unit)
 
         self.bands = self._init_bands(bands)
 
@@ -104,23 +113,26 @@ class Lightcurve_data():
         return sigma_errors
 
     def _init_lightcurve_data(self):
-        lightcurve_data = dict((band, self._get_lightcurves(band)) for band in self.bands)
+        lightcurve_data = dict((self.bands[band_index], self._get_lightcurves(band_index)) for band_index in xrange(len(self.bands)))
         return lightcurve_data
 
-    def _get_lightcurves(self, band):
-        lightcurves = Lightcurve_collection(self._get_baseline_measurements(band),
-                                               self._get_event_curve(band),
-                                               self._get_event_measurements(band),
+    def _get_lightcurves(self, band_index):
+        band = self.bands[band_index]
+        lightcurves = Lightcurve_collection(self._get_baseline_measurements(band_index),
+                                               self._get_event_curve(band_index),
+                                               self._get_event_measurements(band_index),
                                                band=band)
         return lightcurves
 
-    def _get_baseline_measurements(self, band):
-        baseline_measurements = Lightcurve(None, None, mag_errors=None, band=band)
+    def _get_baseline_measurements(self, band_index):
+        baseline_measurements = self._get_measurements(band_index, get_baseline=True)
+
         return baseline_measurements
 
-    def _get_event_curve(self, band):
+    def _get_event_curve(self, band_index):
         duration_val = self.duration.to(self.time_unit).value
         time_step_val = self.time_step.to(self.time_unit).value
+        band = self.bands[band_index]
         mag = self.mags[band]
 
         times = np.arange(0, duration_val, time_step_val) * self.time_unit
@@ -131,16 +143,101 @@ class Lightcurve_data():
         event_curve = Lightcurve(times, mags, band=band)
         return event_curve
 
-    def _get_event_measurements(self, band):
-        event_measurements = Lightcurve(None, None, mag_errors=None, band=band)
+    def _get_event_measurements(self, band_index):
+        event_measurements = self._get_measurements(band_index, get_baseline=False)
         return event_measurements
 
-    def plot_event_curve(self, band, fmt="--r"):
+    def _get_measurements(self, band_index, get_baseline=False):
+        band_period = (self.period * len(self.bands)).to(self.time_unit)
+        band_start_time = ((self.period * band_index) + self.start_time).to(self.time_unit)
+        band = self.bands[band_index]
+        baseline_mag = self.mags[band]
+
+        time_observing = 0 * self.time_unit
+        true_times = []
+        gaussian_mags = []
+        gaussian_mag_errors = []
+        while True:
+            true_time = get_true_observation_time(time_observing, start_time=band_start_time,
+                                                  night_duration=self.night_duration,
+                                                  day_night_duration=self.day_night_duration)
+            if true_time > self.duration:
+                break
+
+            """If we are getting baseline measurements, we randomize the
+            baseline magnitude;
+            If we are getting measurements of a microlensing event, we magnify
+            the baseline magnitude before randomizing it
+            """
+            if get_baseline:
+                mag = baseline_mag
+            else:
+                mag = get_magnified_mag(baseline_mag, self.impact_min, true_time, self.time_max, self.einstein_time)
+
+            gaussian_mag_info = get_gaussian_mag_info(mag, debug=False)
+            gaussian_mag = gaussian_mag_info["gaussian_mag"]
+            mag_sigma_error = gaussian_mag_info["mag_err"]
+            gaussian_mag_error = gaussian_mag_info["gaussian_mag_err"]
+            """
+            if mag_sigma_error >= MAG_ERROR_THRESHOLD:
+                print("Warning: sigma error {} for magnitude {} exceeds threshold {}".format(
+                      mag_sigma_error, mag, MAG_ERROR_THRESHOLD))
+                print("Omitting this data point, where (true) time")
+            """
+            true_times.append(true_time)
+            gaussian_mags.append(gaussian_mag)
+            gaussian_mag_errors.append(gaussian_mag_error)
+
+            time_observing += band_period
+
+        # convert to array Quantities, rather than lists of Quantities
+        if true_times:
+            true_times = units.Quantity(true_times)
+        if gaussian_mags:
+            gaussian_mags = units.Quantity(gaussian_mags)
+        if gaussian_mag_errors:
+            gaussian_mag_errors = units.Quantity(gaussian_mag_errors)
+
+        measurements = Lightcurve(true_times, gaussian_mags,
+                                           mag_errors=gaussian_mag_errors, band=band)
+        print band
+        print measurements.times
+        print
+        return measurements
+
+
+    def _set_plot_title(self, band):
         plt.title("band: {}     u0: {}    t_E: {}     t_max: {}".format(band,
                                                                         self.impact_min,
                                                                         self.einstein_time,
                                                                         self.time_max))
+
+    def plot_all(self, band):
+        self.plot_event_curve(band)
+        self.plot_baseline_measurements(band)
+        self.plot_event_measurements(band)
+
+    def plot_event_curve(self, band, fmt=None):
+        if fmt is None:
+            plot_color = BAND_PLOT_COLOR_DICT[band]
+            fmt = plot_color + "--"
+
+        self._set_plot_title(band)
         self.lightcurve_data[band].event_curve.plot(fmt=fmt, set_title=False)
+
+    def plot_baseline_measurements(self, band, fmt=None):
+        if fmt is None:
+            plot_color = BAND_PLOT_COLOR_DICT[band]
+            fmt = plot_color + "^"
+        self._set_plot_title(band)
+        self.lightcurve_data[band].baseline_measurements.plot(fmt=fmt, set_title=False)
+
+    def plot_event_measurements(self, band, fmt=None):
+        if fmt is None:
+            plot_color = BAND_PLOT_COLOR_DICT[band]
+            fmt = plot_color + "o"
+        self._set_plot_title(band)
+        self.lightcurve_data[band].event_measurements.plot(fmt=fmt, set_title=False)
 
     @staticmethod
     def display_plots():
@@ -166,13 +263,35 @@ class Lightcurve():
             plt.title("band: {}".format(self.band))
         plt.xlabel("time ({})".format(self.times.unit))
         plt.ylabel("magnitude")
-        plt.errorbar(self.times, self.mags, yerr=self.mag_errors, fmt=fmt)
+
+        times = self.times.value
+        mags = self.mags.value
+        if self.mag_errors:
+            mag_errors = self.mag_errors.value
+        else:
+            mag_errors = None
+
+        plt.errorbar(times, mags, yerr=mag_errors, fmt=fmt)
 
 def test_Lightcurve_data():
-    lightcurve_data = Lightcurve_data(mags={"V": 25}, einstein_time=1*units.d, time_max=25*units.d)
+    lightcurve_data = Lightcurve_data(mags={"V": 25, "B": 20, "U": 22, "I": 23, "K": 28}, einstein_time=3*units.d,
+                                      time_max=5 * units.d, duration=5*units.d, period=17.7 / 5* units.h,
+                                      time_unit=units.d)
+
+    """
+    for band in lightcurve_data.bands:
+        lightcurve_data.plot_event_curve(band)
+        lightcurve_data.plot_baseline_measurements(band)
+        lightcurve_data.plot_event_measurements(band)
+
+    print lightcurve_data.duration
+    for band in lightcurve_data.bands:
+        print band + ":", BAND_PLOT_COLOR_DICT[band]
+    """
+
     lightcurve_data.plot_event_curve("V")
-    Lightcurve_data.display_plots()
-    lightcurve_data.lightcurve_data["V"].event_curve.plot()
+    li
+
     Lightcurve_data.display_plots()
 
 def main():
--- true_observation_time.py
+++ true_observation_time.py
@@ -12,16 +12,18 @@ def get_true_observation_time(obs_number, period, night_duration, day_night_dura
     return observation_time
 """
 
-def get_true_observation_time(time, start_time_offset=0*units.h,
+def get_true_observation_time(time, start_time=0*units.h,
                               night_duration=10*units.h, day_night_duration=24*units.h):
-    observation_time = int(time / night_duration) * day_night_duration \
-                       + time % night_duration
+    observation_time = int((time + start_time)/ night_duration) * day_night_duration \
+                       + (time + start_time) % night_duration
 
     return observation_time
 
 def get_true_observation_times(start_time = 0*units.h, duration=154*units.h, period=17.7*units.h,
                           night_duration=10*units.h, day_night_duration=24*units.h):
     # start_time is not working properly
+    # actually: start_time is the number of nighttime hours that pass before
+    # observing begins, with time 0 being fixed as the start of a night
 
     if night_duration > day_night_duration:
         print "WARNING: Night duration is longer than day+night parameter."
@@ -32,13 +34,14 @@ def get_true_observation_times(start_time = 0*units.h, duration=154*units.h, per
     obs_number = 0
 
     while True:
-        time_observing = obs_number * period + start_time
-        time = get_true_observation_time(time_observing,
+        time_observing = obs_number * period
+        time = get_true_observation_time(time_observing, start_time=start_time,
                                          night_duration=night_duration,
                                          day_night_duration=day_night_duration)
 
-        print (obs_number * period), time
-        if time >= start_time + duration:
+        print time_observing, time
+        #if time >= start_time + duration:
+        if time >= duration:
             break
         else:
             time_list.append(time)
@@ -53,7 +56,8 @@ def get_true_observation_times(start_time = 0*units.h, duration=154*units.h, per
 def main():
     #time_list = get_true_observation_times(220, 17.7, 10, 24)
     #time_list = get_true_observation_times(duration=220*units.h, night_duration = 24*units.h)
-    time_list = get_true_observation_times(duration=220*units.h)
+    period = 17.7*units.h
+    time_list = get_true_observation_times(period=period*5, duration=550*units.h, start_time=period*1)
     print(time_list)
 
 if __name__ == "__main__":
